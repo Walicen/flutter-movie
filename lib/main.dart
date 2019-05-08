1b0910960298da2d2f5a95634004bbf3ff7388aa
Walicen <walicen.r@gmail.com>
2019-05-07 23:27:23 -0300

    Iniciando o projeto

---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+  import 'package:flutter/material.dart';
+
+class MovieApp extends StatelessWidget {
+    @override
+    Widget build(BuildContext context) {
+      return MaterialApp(
+        home: Container(),
+      );
+    }
+  }
