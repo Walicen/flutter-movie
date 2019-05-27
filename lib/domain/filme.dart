169ce5d66f490b036ea90ade43b1e344ea9b16e0
Walicen <walicen.r@gmail.com>
2019-05-26 23:02:08 -0300

    title and name serie/movie

--- lib/domain/filme.dart
+++ lib/domain/filme.dart
@@ -3,14 +3,16 @@ import 'dart:convert';
 class Filme {
 
   String title;
+  String name; //Series
   String poster_path;
   String overview;
   String backdrop_path;
 
-  Filme(this.title, this.poster_path, this.overview, this.backdrop_path);
+  Filme(this.title, this.name, this.poster_path, this.overview, this.backdrop_path);
 
   Filme.fromJson(Map<String,dynamic> map):
         title = map["title"],
+        name = map["name"],
         poster_path = map["poster_path"],
         overview = map["overview"],
         backdrop_path = map["backdrop_path"];
@@ -18,6 +20,7 @@ class Filme {
   toMap() {
     return {
       "title": title,
+      "name": name,
       "poster_path": poster_path,
       "overview": overview,
       "backdrop_path": backdrop_path,
--- lib/pages/detail_movie_page.dart
+++ lib/pages/detail_movie_page.dart
@@ -22,37 +22,54 @@ class _DatailFilmePageState extends State<DatailFilmePage> {
   }
 
   _buildBody(Filme filme) {
-    return Stack(
+    return ListView(
+      shrinkWrap: true,
       children: <Widget>[
-        Positioned.fill(
-          child: Container(
-            child: Image.network(
-              FilmesService.url_image + filme.backdrop_path,
-              fit: BoxFit.fitHeight,
-            ),
-          ),
+        Container(
+
+          color: Colors.white,
+          child: _imageBack(filme),
         ),
         Container(
-          child: SafeArea(
-            child: Padding(
-              padding: const EdgeInsets.all(12.0),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: <Widget>[
-                  Icon(
-                    Icons.arrow_back,
-                    color: Colors.white,
-                  ),
-                  Icon(
-                    Icons.settings,
-                    color: Colors.white,
-                  ),
-                ],
-              ),
-            ),
+          child: Text(filme.title != null ? filme.title : filme.name, style: TextStyle(fontSize: 40, color: Colors.green),),
+        ),
+        Container(
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Text(filme.overview, style: TextStyle(fontSize: 15, color: Colors.green),),
           ),
         ),
       ],
+    );
+  }
+
+   _imageBack(Filme filme) {
+    return Image.network(
+      FilmesService.url_image + filme.backdrop_path,
+      fit: BoxFit.fitHeight,
+    );
+  }
+
+  _myAppBar() {
+    return Container(
+      child: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(12.0),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              Icon(
+                Icons.arrow_back,
+                color: Colors.white,
+              ),
+              Icon(
+                Icons.settings,
+                color: Colors.white,
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
